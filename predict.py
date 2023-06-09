30fb9ddb449c21b90fef347addcbbffec1b57fdf
Victor Thuillier <victor@Users-MacBook-Pro.local>
2023-06-09 10:50:14 +0200

    Fix

--- predict.py
+++ predict.py
@@ -31,25 +31,6 @@ class Predictor(BasePredictor):
         audio = whisperx.load_audio(audio_file)
         result = model.transcribe(audio, batch_size=batch_size)
 
-        # delete model if low on GPU resources
-        # import gc; gc.collect(); torch.cuda.empty_cache(); del model
-
-        # 2. Align whisper output
-        #model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
-        #result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
-
-        # delete model if low on GPU resources
-        # import gc; gc.collect(); torch.cuda.empty_cache(); del model_a
-
-        # 3. Assign speaker labels
-        #diarize_model = whisperx.DiarizationPipeline(use_auth_token="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", device=device)
-
-        # add min/max number of speakers if known
-        #diarize_segments = diarize_model(audio_file)
-        # diarize_model(audio_file, min_speakers=min_speakers, max_speakers=max_speakers)
-
-        #result = whisperx.assign_word_speakers(diarize_segments, result)
-
         return ModelOutput(
             segments=result["segments"],
             detected_language=result["language"]
