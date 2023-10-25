d964e67ac5ff10dba4510d676732c2f32d877bcc
Victor Thuillier <victor@Users-MacBook-Pro.local>
2023-10-25 09:44:00 +0200

    fix

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e
+
 model_dir=models/fast-whisper-large-v2
 
 mkdir -p $model_dir
 
-wget -O $model_dir https://huggingface.co/guillaumekln/faster-whisper-large-v2/resolve/main/config.json
-wget -O $model_dir https://huggingface.co/guillaumekln/faster-whisper-large-v2/resolve/main/model.bin
-wget -O $model_dir https://huggingface.co/guillaumekln/faster-whisper-large-v2/resolve/main/tokenizer.json
-wget -O $model_dir https://huggingface.co/guillaumekln/faster-whisper-large-v2/resolve/main/vocabulary.txt
+wget -O $model_dir/config.json https://huggingface.co/guillaumekln/faster-whisper-large-v2/resolve/main/config.json
+wget -O $model_dir/model.bin https://huggingface.co/guillaumekln/faster-whisper-large-v2/resolve/main/model.bin
+wget -O $model_dir/tokenizer.json https://huggingface.co/guillaumekln/faster-whisper-large-v2/resolve/main/tokenizer.json
+wget -O $model_dir/vocabulary.txt https://huggingface.co/guillaumekln/faster-whisper-large-v2/resolve/main/vocabulary.txt
 
 cog run python
