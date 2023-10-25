cf7744f7c9c68621f89fe5695ab160568fb8fc46
Victor Thuillier <victor@Users-MacBook-Pro.local>
2023-10-25 10:40:55 +0200

    fix

--- build.sh
+++ build.sh
@@ -11,6 +11,8 @@ wget -O $faster_whisper_model_dir/model.bin https://huggingface.co/guillaumekln/
 wget -O $faster_whisper_model_dir/tokenizer.json https://huggingface.co/guillaumekln/faster-whisper-large-v2/resolve/main/tokenizer.json
 wget -O $faster_whisper_model_dir/vocabulary.txt https://huggingface.co/guillaumekln/faster-whisper-large-v2/resolve/main/vocabulary.txt
 
+pip install -u git+https://github.com/m-bain/whisperx.git
+
 vad_model_dir=models/vad
 
 mkdir -p $vad_model_dir
