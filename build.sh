86d8d9f341f958fdc6499553f308d024fea30fd9
Victor Thuillier <victor@Users-MacBook-Pro.local>
2024-02-15 11:16:05 +0100

    fix

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ download "https://huggingface.co/Systran/faster-whisper-large-v3/resolve/main/co
 download "https://huggingface.co/Systran/faster-whisper-large-v3/resolve/main/model.bin" "$faster_whisper_model_dir/model.bin"
 download "https://huggingface.co/Systran/faster-whisper-large-v3/resolve/main/preprocessor_config.json" "$faster_whisper_model_dir/preprocessor_config.json"
 download "https://huggingface.co/Systran/faster-whisper-large-v3/resolve/main/tokenizer.json" "$faster_whisper_model_dir/tokenizer.json"
-download "https://huggingface.co/Systran/faster-whisper-large-v3/resolve/main/vocabulary.json" "$faster_whisper_model_dir/vocabulary.txt"
+download "https://huggingface.co/Systran/faster-whisper-large-v3/resolve/main/vocabulary.json" "$faster_whisper_model_dir/vocabulary.json"
 
 pip install -U git+https://github.com/m-bain/whisperx.git
 
