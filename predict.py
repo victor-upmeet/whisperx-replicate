394a12dd3bbcd67a26e9645594fe3c92b12c2fab
sonicbaume <102724304+sonicbaume@users.noreply.github.com>
2024-08-04 13:08:53 +0100

    Removed model_name from DiarizationPipeline to use latest pyannote model.

--- predict.py
+++ predict.py
@@ -280,8 +280,7 @@ def align(audio, result, debug):
 def diarize(audio, result, debug, huggingface_access_token, min_speakers, max_speakers):
     start_time = time.time_ns() / 1e6
 
-    diarize_model = whisperx.DiarizationPipeline(model_name='pyannote/speaker-diarization@2.1',
-                                                 use_auth_token=huggingface_access_token, device=device)
+    diarize_model = whisperx.DiarizationPipeline(use_auth_token=huggingface_access_token, device=device)
     diarize_segments = diarize_model(audio, min_speakers=min_speakers, max_speakers=max_speakers)
 
     result = whisperx.assign_word_speakers(diarize_segments, result)
